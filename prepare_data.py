ea477bc9f18c6a40412d9fca5472be60f3205641
MadsPoder <MadsPoder@users.noreply.github.com>
2018-04-18 17:05:56 +0200

    Better gtsdb conversion script

--- prepare_data.py
+++ prepare_data.py
@@ -1,40 +1,45 @@
 import os
 from PIL import Image
 
+#Paths GTSDB
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FILES_PATH = os.path.join(BASE_DIR, 'FullIJCNN2013')
 CLEANED_PATH = os.path.join(BASE_DIR, 'FullIJCNN2013_converted')
+GROUND_TRUTH = os.path.join(FILES_PATH,"gt.txt")
+GROUND_TRUTH_CONVERTERD = os.path.join(CLEANED_PATH,"gt.csv")
+CLASS_MAPPINGS = os.path.join(CLEANED_PATH,"cm.csv")
 
-def convert_images():
-    print("Converting images")
-    files = [f for f in os.listdir(FILES_PATH) if os.path.splitext(f)[1] == ".ppm"]
-    
-    for file in files:
+def convert_gtsdb_images():
+    print("--------- Converting GTSDB Images ---------")
+    files = [file for file in os.listdir(FILES_PATH) if os.path.splitext(file)[1] == ".ppm"]
+
+    for idx, file in enumerate(files):
+        print("Converting image {} of {}".format(idx, len(files)-1), end='\r')
         #Converting from ppm to jpg using pillow https://stackoverflow.com/a/27046228
         im = Image.open(os.path.join(FILES_PATH, file))
         im.save(os.path.join(CLEANED_PATH, os.path.splitext(file)[0]+".png"))
 
-if not os.path.exists(CLEANED_PATH):
-    os.makedirs(CLEANED_PATH)
-
-def create_labels():
-    print("Creating labels")
+def convert_gtsdb_labels():
+    print("--------- Converting GTSDB Labels ---------")
     parsed = []
 
     #Ground truth conversion
-    with open(os.path.join(FILES_PATH,"gt.txt"), "r") as fp:
+    with open(GROUND_TRUTH, "r") as fp:
         for line in fp:
             parsed.append(CLEANED_PATH+"/"+line.replace(";",",").replace("ppm","png"))
     
-    with open(os.path.join(CLEANED_PATH,"gt.csv"), "w") as fp:
+    with open(GROUND_TRUTH_CONVERTERD, "w") as fp:
         for line in parsed:
             fp.write(line)
     
     #Class mappings (for now just numbers)
-    with open(os.path.join(CLEANED_PATH,"cm.csv"), "w") as fp:
+    with open(CLASS_MAPPINGS, "w") as fp:
         for number in list(range(43)):
             fp.write(str(number)+","+str(number)+"\n")
 
+if not os.path.exists(CLEANED_PATH):
+    os.makedirs(CLEANED_PATH)
 
-#convert_images()
-create_labels()
+#GTSDB
+#convert_gtsdb_images()
+#convert_gtsdb_labels()
